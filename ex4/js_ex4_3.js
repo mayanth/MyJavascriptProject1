c40e71eb9f6573ffced3316150857ca9cffbaa96
mayanth <anothai.ssht@gmail.com>
2022-09-19 14:40:35 +0700

    js_ex5

--- ex4/js_ex4_3.js
+++ ex4/js_ex4_3.js
@@ -1,4 +1,5 @@
-//Ex4.3: Javascript Variables and Operators
-let raw_score = 180;
+//JavaScript by Aj.NesT the Series 
+//Ex4.3: JavaScript Variables and Operators
+let raw_score = 180;  //let -> use only { } block scope
 document.getElementById("ex4-3-1").innerHTML = raw_score;
 document.getElementById("ex4-3-2").innerHTML = "score: "+(raw_score*100)/200;
