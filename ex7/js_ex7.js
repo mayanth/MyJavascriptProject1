e33af4932376693e137f5a7ff73ce5d3e018ae30
mayanth <anothai.ssht@gmail.com>
2022-09-19 15:18:25 +0700

    js_ex7

---
+++ ex7/js_ex7.js
@@ -0,0 +1,10 @@
+//Ex7: Javascript String Concatenation
+var text1 = "I'm studying";
+var text2 = "Javascript Programming";
+var text3 = "Visual Studio Code 😍";
+document.getElementById("ex7").innerHTML = text1+text2+" with"+text3;
+text1 += "Python Programming 😊";
+document.write(text1);
+let text4 = "📆" + "Mon" + 4;
+let text5 = "October" + 2021;
+document.write("<br><br>"+text4+" "+text5);
